513d31b1bfcd7dfc64b0d55ff5b5fdb313bdb19a
arvind <negiarvind229@gmail.com>
2018-06-25 20:53:38 +0530

    splash added comment

--- app/src/main/java/topnotes/nituk/com/topnotes/LoginActivity.java
+++ app/src/main/java/topnotes/nituk/com/topnotes/LoginActivity.java
@@ -10,5 +10,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //Splash activity
     }
 }
