57d75b0fe88a2ea7e8017a63b7c39de0d2486303
Thesohan <sohan.cse16@nituk.ac.in>
2018-06-25 20:56:38 +0530

    Merge branch 'master' of https://github.com/akraturi/TopNotes
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/vcs.xml

--- app/src/main/java/topnotes/nituk/com/topnotes/LoginActivity.java
+++ app/src/main/java/topnotes/nituk/com/topnotes/LoginActivity.java
@@ -2,7 +2,9 @@ package topnotes.nituk.com.topnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 public class LoginActivity extends AppCompatActivity {
 
--- app/src/main/java/topnotes/nituk/com/topnotes/SplashActivity.java
+++ app/src/main/java/topnotes/nituk/com/topnotes/SplashActivity.java
@@ -2,7 +2,9 @@ package topnotes.nituk.com.topnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 // Sohan create a splash of 4 sec which leads to the login activity
 
@@ -12,5 +14,21 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new CountDownTimer(4000,1000){
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+
+            }
+        }.start();
+
+
+
     }
 }
