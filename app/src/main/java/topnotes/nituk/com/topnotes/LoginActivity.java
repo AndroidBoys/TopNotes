cb73465c8ff2455797eedd59aab2b5a7f5e56555
arvind <negiarvind229@gmail.com>
2018-06-26 11:10:00 +0530

    Navigation Drawer added

--- app/src/main/java/topnotes/nituk/com/topnotes/LoginActivity.java
+++ app/src/main/java/topnotes/nituk/com/topnotes/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +13,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //This below codde is just for testing purpose.
+        //Don't need to add this one into your project
+        Intent intent=new Intent(LoginActivity.this,SubjectListActivity.class);
+        startActivity(intent);
+
     }
 }
