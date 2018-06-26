80fda1a3dac4da776cacada3c331b4cfc0fafe43
akraturi <mhraturi@gmail.com>
2018-06-26 10:05:23 +0530

    * Firebase core is integrated * resultreciever already present conflict is ressolved(androidx conflict):app in running state * actionbar is depricated from the splash screen * UI changes in LoginActivity

--- app/src/main/java/topnotes/nituk/com/topnotes/SplashActivity.java
+++ app/src/main/java/topnotes/nituk/com/topnotes/SplashActivity.java
@@ -14,6 +14,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        // Action bar is depricated from the splash
+        getSupportActionBar().hide();
+
         new CountDownTimer(4000,1000){
             @Override
             public void onTick(long l) {
