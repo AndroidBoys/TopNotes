3af4448d9d867a00d282a48b1ba6178c42d55dad
Thesohan <sohan.cse16@nituk.ac.in>
2018-06-27 09:40:37 +0530

    nothing for today/./////

--- app/src/main/java/topnotes/nituk/com/topnotes/Dialog_fragment.java
+++ app/src/main/java/topnotes/nituk/com/topnotes/Dialog_fragment.java
@@ -11,7 +11,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
-public class Dialog_fragment extends DialogFragment {
+public class Dialog_fragment extends DialogFragment implements View.OnClickListener {
+
+    protected TextView notesTextView,questionPaperTextView,resourceTextView,practicalFileTextView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -19,16 +21,40 @@ public class Dialog_fragment extends DialogFragment {
             //by using view you can find the id of the widgets present in R.layout.dialog_fragment
         View view=inflater.inflate(R.layout.dialog_fragment,container,false);
 
-//        TextView tv=view.findViewById(R.id.notes);
-//        tv.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(getActivity(), "hello sdfasd", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        notesTextView=view.findViewById(R.id.notes);
+        questionPaperTextView=view.findViewById(R.id.questionPaper);
+        resourceTextView=view.findViewById(R.id.resources);
+        practicalFileTextView=view.findViewById(R.id.practicalFiles);
+
+        notesTextView.setOnClickListener(this);
+        notesTextView.setOnClickListener(this);
+        notesTextView.setOnClickListener(this);
+        notesTextView.setOnClickListener(this);
         return view;
     }
 
     public Dialog_fragment() {
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.notes:
+                Toast.makeText(getActivity(), "notes", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.resources:
+                Toast.makeText(getActivity(), "notes", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.practicalFiles:
+                Toast.makeText(getActivity(), "notes", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.questionPaper:
+                Toast.makeText(getActivity(), "notes", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
     }
 }
