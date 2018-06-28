f125d43b41267bcff3d56a021d57240c55149db4
arvind <negiarvind229@gmail.com>
2018-06-28 19:31:02 +0530

    something

--- app/src/main/java/topnotes/nituk/com/topnotes/SubjectListActivity.java
+++ app/src/main/java/topnotes/nituk/com/topnotes/SubjectListActivity.java
@@ -94,7 +94,7 @@ public class SubjectListActivity extends AppCompatActivity {
                 drawerLayout.closeDrawer(Gravity.START);
                 return true;
             }
-        });
+        });//
 
 
     }
