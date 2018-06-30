f76cec7fb9921743c962b65090ab6e2615b61776
Thesohan <sohan.cse16@nituk.ac.in>
2018-07-01 03:57:44 +0530

    comment added and DbHelper modified........

--- app/src/main/java/topnotes/nituk/com/topnotes/ContentsActivity.java
+++ app/src/main/java/topnotes/nituk/com/topnotes/ContentsActivity.java
@@ -22,6 +22,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import java.util.ArrayList;
 import java.util.List;
